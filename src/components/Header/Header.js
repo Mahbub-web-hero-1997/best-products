05225fb767baa0f89b02ae2ea62673f97755fa68
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2022-04-03 23:01:33 +0600

    navbar and router added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='container'>
+            <nav className='nav_Container'>
+                <div className='Logo_Container'>
+                    <h1>BEST-PRODUCT</h1>
+                </div>
+                <div className='link_Container'>
+                    <Link className='link' to='/Home'>Home</Link>
+                    <Link className='link' to='/reviews'>Reviews</Link>
+                    <Link className='link' to='/dashboard'>Dashboard</Link>
+                    <Link className='link' to='/about'>About</Link>
+                </div>
+
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
